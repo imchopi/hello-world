137e6735bbdae8b48d6f27735a3d4e9470131af4
imchopi <apchopito@gmail.com>
2023-09-30 10:33:55 +0200

    Card per second added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,26 +9,29 @@ import { User } from './user';
 })
 
 export class HomePage implements OnInit {
+	private _users:BehaviorSubject<User[]> = new BehaviorSubject<User[]>([]);
+	users$:Observable<User[]> = this._users.asObservable();
 
-  private _users:BehaviorSubject<User[]> = new BehaviorSubject<User[]>([]);
-  
-  users$:Observable<User[]> = this._users.asObservable();
+  constructor() {}
 
-  users = [
-    { nombre: 'Adrián', apellido: 'Perogil', edad: 27},
-    { nombre: 'Daniel', apellido: 'Ávila', edad: 19},
-    { nombre: 'Diego', apellido: 'León', edad: 21},
-    { nombre: 'Javier', apellido: 'Antidisturbio', edad: 82},
-    { nombre: 'Marco', apellido: 'Pete', edad: 22},
-  ]
-
-  constructor() {
-  }
-  
-  ngOnInit(): void {
-    setTimeout(() => {
-      this._users.next(this.users); 
-    }, 1000);
-     
-  }
+	// En el método ngOnInit del componente
+	ngOnInit(): void {
+    let users: User[] = [
+      { nombre: 'Adrián', apellido: 'Perogil', edad: 27},
+      { nombre: 'Daniel', apellido: 'Ávila', edad: 19},
+      { nombre: 'Diego', apellido: 'León', edad: 21},
+      { nombre: 'Javier', apellido: 'Antidisturbio', edad: 82},
+      { nombre: 'Marco', apellido: 'Pete', edad: 22},
+    ]
+    var index = 0
+	  // Utilizamos setInterval para agregar usuarios adicionales al BehaviorSubject cada segundo
+	  setInterval(() => {
+	    if (index < 5) {
+	      let user: User[] = this._users.value; // Obtenemos la lista actual de usuarios
+	      user.push(users[index]); // Agregamos un usuario de la lista actual al final
+	      this._users.next(user); // Actualizamos el BehaviorSubject con la nueva lista
+	      index++; // Incrementamos el índice para seleccionar el próximo usuario
+	    }
+	  }, 1000);
+	}
 }
